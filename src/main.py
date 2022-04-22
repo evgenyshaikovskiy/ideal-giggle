91d7ecb0dac21d0b84c08e73feef8b3c4ab44882
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-04-23 01:42:13 +0300

    update main script

---
+++ src/main.py
@@ -0,0 +1,43 @@
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
+
+from contoller.controller import ControllerComponent
+from model.model import ModelComponent
+
+from kivy.core.window import Window
+from kivy.metrics import dp
+
+
+class RunApplication(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.table = MDDataTable(
+            pos_hint={'center_x': 0.5,'center_y': 0.54},
+            size_hint={0.9, 0.8},
+            use_pagination=True,
+            rows_num=12,
+            column_data=[
+                ('Full name', dp(30)),
+                ('Group', dp(20)),
+                ('1', dp(10)),
+                ('2', dp(10)),
+                ('3', dp(10)),
+                ('4', dp(10)),
+                ('5', dp(10)),
+                ('6', dp(10)),
+                ('7', dp(10)),
+                ('8', dp(10)),
+                ('9', dp(10)),
+                ('10', dp(10)),
+            ],
+        )
+        self.model = ModelComponent(table=self.table)
+        self.controller = ControllerComponent(self.model)
+        
+    
+    def build(self):
+        Window.size = (1000, 800)
+        return self.controller.get_screen()
+    
+
+RunApplication().run()
