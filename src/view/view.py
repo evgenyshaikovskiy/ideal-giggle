5571f126d24a0552670ecab974b9203b35613993
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-04-23 11:10:34 +0300

    refactor view component

--- src/view/view.py
+++ src/view/view.py
@@ -17,18 +17,18 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.screen import Screen
 from kivymd.uix.snackbar import Snackbar
 
+
 class ViewComponent(MDScreen):
     controller = ObjectProperty()
     model = ObjectProperty()
-    
+
     def __init__(self, **kw):
         super().__init__(**kw)
-        
+
         # subscribe view component to model as observer
         self.model.subscribe(self)
         self.screen = Screen()
-    
-    
+
     def open_dialog(self, window_type: str):
         if window_type == 'input':
             self.dialog = InputWindow(model=self.model)
@@ -40,12 +40,11 @@ class ViewComponent(MDScreen):
             self.dialog = SaveWindow(model=self.model)
         elif window_type == 'upload':
             self.dialog = UploadWindow(model=self.model)
-            
+
         self.dialog.open()
         self.controller.dialog(window_type, self.dialog)
-    
-    
-    def close_dialog(self, dialog_data: list=[]):
+
+    def close_dialog(self, dialog_data: list = []):
         if self.dialog.mode == 'input':
             self.controller.input_student(dialog_data)
         elif self.dialog.mode == 'filter':
@@ -57,20 +56,18 @@ class ViewComponent(MDScreen):
             self.controller.upload_from_file(dialog_data)
         elif self.dialog.mode == 'save':
             self.controller.save_in_file(dialog_data)
-        
+
         self.dialog = None
-        
-    
+
     def on_model_change(self, data):
         self.close_dialog(data)
 
-
     def refresh(self):
         self.controller.refresh()
-        
-        
+
     def build(self):
         self.add_widget(self.model.table)
         return self
-    
+
+
 Builder.load_file('/home/evgeny/source/repos/ideal-giggle/src/view/view.kv')
